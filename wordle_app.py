a14595adc15fdb3d6b6eadbe49c5fca446f53553
Shiqi Liang <65426399+sqliang99@users.noreply.github.com>
2022-02-10 09:16:52 -0800

    Update wordle_app.py

--- wordle_app.py
+++ wordle_app.py
@@ -18,31 +18,31 @@ try:
 
     first = st.checkbox('i know the first letter')
     if first:
-        dic['first']=st.text_input('first letter', 'p')
+        dic['first']=st.text_input('first letter', 'c').lower()
 
     second = st.checkbox('i know the second letter')
     if second:
-        dic['second']=st.text_input('second letter', 'e')
+        dic['second']=st.text_input('second letter', 'r').lower()
 
     third = st.checkbox('i know the third letter')
     if third:
-        dic['third']=st.text_input('third letter', 'n')
+        dic['third']=st.text_input('third letter', 'a').lower()
 
     fourth = st.checkbox('i know the fourth letter')
     if fourth:
-        dic['fourth']=st.text_input('fourth letter', 'i')
+        dic['fourth']=st.text_input('fourth letter', 'n').lower()
 
     fifth = st.checkbox('i know the fifth letter')
     if fifth:
-        dic['fifth']=st.text_input('fifth letter', 's')
+        dic['fifth']=st.text_input('fifth letter', 'e').lower()
 
     others = st.checkbox('i know letters that are in the word, but im not sure about the position')
     if others:
-        l = st.text_input('enter with space between the letters', 'a b c')
+        l = st.text_input('enter with space between the letters', 'a b c').lower()
         letters = l.split(' ')
     avoid = st.checkbox('i know letters that are not in the word')
     if avoid:
-        l = st.text_input('enter these letter with space between', 'a b c')
+        l = st.text_input('enter these letter with space between', 'a b c').lower()
         avoidlist = l.split(' ')
     out = fw.find_wordle(avoidlist,*letters,**dic)
     st.write(out)
